5c25f33c16b6cb92833704882f25f732120b135b
yuvron <yuvr99@gmail.com>
2022-12-10 00:44:11 +0200

    Receive input from user and mark correct attempts

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -2,16 +2,25 @@ import "./Word.scss";
 
 interface WordProps {
 	finalWord: string;
+	currentText: string;
 }
 
-const Word: React.FC<WordProps> = ({ finalWord }) => {
+const Word: React.FC<WordProps> = ({ finalWord, currentText }) => {
 	return (
 		<div className="word">
-			<span className="letter"></span>
-			<span className="letter"></span>
-			<span className="letter"></span>
-			<span className="letter"></span>
-			<span className="letter"></span>
+			{[...Array(5)].map((element, index) => {
+				let outcome = "";
+				if (finalWord) {
+					if (currentText[index]) outcome = "wrong";
+					if (finalWord.includes(currentText[index])) outcome = "part-correct";
+					if (finalWord[index] === currentText[index]) outcome = "correct";
+				}
+				return (
+					<span key={index} className={`letter ${outcome}`}>
+						{currentText[index]}
+					</span>
+				);
+			})}
 		</div>
 	);
 };
--- src/components/WordsList/WordsList.tsx
+++ src/components/WordsList/WordsList.tsx
@@ -4,20 +4,36 @@ import "./WordsList.scss";
 
 const WordsList: React.FC = () => {
 	const [finalWord, setFinalWord] = useState("");
+	const [currentWord, setCurrentWord] = useState(0);
+	const [attemptsList, setAttemptsList] = useState(["", "", "", "", "", ""]);
 
 	useEffect(() => {
-		const words = require("word-list-json").filter((word: string) => word.length === 6);
+		const words = require("word-list-json").filter((word: string) => word.length === 5);
 		setFinalWord(words[Math.floor(Math.random() * words.length)]);
+		console.log(finalWord);
 	}, []);
+
+	useEffect(() => {
+		document.addEventListener("keypress", handleKeyPress);
+		return () => {
+			document.removeEventListener("keypress", handleKeyPress);
+		};
+	}, [attemptsList]);
+
+	const handleKeyPress = (e: KeyboardEvent) => {
+		if (currentWord === 6) return;
+		const { key } = e;
+		const newAttemptsList = [...attemptsList];
+		newAttemptsList[currentWord] += key;
+		if (newAttemptsList[currentWord].length === 5) setCurrentWord(currentWord + 1);
+		setAttemptsList(newAttemptsList);
+	};
 
 	return (
 		<div className="words-list">
-			<Word finalWord={finalWord} />
-			<Word finalWord={finalWord} />
-			<Word finalWord={finalWord} />
-			<Word finalWord={finalWord} />
-			<Word finalWord={finalWord} />
-			<Word finalWord={finalWord} />
+			{attemptsList.map((attempt, index) => {
+				return <Word key={index} finalWord={finalWord} currentText={attempt} />;
+			})}
 		</div>
 	);
 };
